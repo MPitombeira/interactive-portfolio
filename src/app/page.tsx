e67530d63290b925d3a2d864c19ba584dcc37c7c
MPitombeira <mateuspitombeirasilva267@gmail.com>
2026-04-01 21:16:43 -0300

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">
+        Welcome, Player
+      </h1>
+    </main>
+  );
+}
