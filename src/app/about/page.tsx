d58600ff4548221adb65d7cf737b65bafc967979
MPitombeira <mateuspitombeirasilva267@gmail.com>
2026-04-03 05:39:50 -0300

    criando a pagina sobre e add traducoes para a mesma

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -54,18 +54,16 @@ export default function About() {
     };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center text-white px-6">
-      <h2>{t.about}</h2>
-
+    <div className="min-h-screen text-white px-6 pt-20 pb-12 flex flex-col items-center">
       <div className="mb-6">
         <div
           className={`w-75 h-75 rounded-none border-4 flex items-center justify-center overflow-hidden
-      ${path === "pyromancer" && "border-red-500 shadow-[0_0_25px_rgba(255,0,0,0.6)]"}
-      ${path === "knight" && "border-blue-500 shadow-[0_0_25px_rgba(0,0,255,0.6)]"}
-      ${path === "cleric" && "border-green-500 shadow-[0_0_25px_rgba(0,255,0,0.6)]"}
-      ${!path && "border-white"}
-      hover:scale-105 transition-transform duration-300
-    `}
+            ${path === "pyromancer" && "border-red-500 shadow-[0_0_25px_rgba(255,0,0,0.6)]"}
+            ${path === "knight" && "border-blue-500 shadow-[0_0_25px_rgba(0,0,255,0.6)]"}
+            ${path === "cleric" && "border-green-500 shadow-[0_0_25px_rgba(0,255,0,0.6)]"}
+            ${!path && "border-white"}
+            hover:scale-105 transition-transform duration-300
+          `}
         >
           <img
             src="/images/me.jfif"
@@ -80,13 +78,8 @@ export default function About() {
       </h1>
 
       <h2 className={`text-xl mb-6 ${colors[path]?.replace("bg", "text")}`}>
-        {path ? classes[path] : "Wandering Developer"}
+        {path ? classes[path] : "Hollow Developer"}
       </h2>
-
-      <p className="max-w-xl text-center mb-10 text-gray-300">
-        Passionate developer focused on creating interactive and immersive web
-        experiences.
-      </p>
 
       <div className="w-full max-w-md mb-8">
         <div className="flex justify-between text-sm mb-1 text-gray-400">
@@ -119,6 +112,126 @@ export default function About() {
           ))}
         </div>
       )}
+
+      <div className="min-h-screen text-white px-6 py-12 flex flex-col items-center">
+        {/* INTRO */}
+        <p className="text-gray-300 max-w-2xl text-center mb-10 leading-relaxed whitespace-pre-line">
+          {t.aboutText}
+        </p>
+
+        {/* EXPERIÊNCIA */}
+        <div className="max-w-2xl w-full mb-10">
+          <h2 className="text-2xl mb-4 text-white-400">{t.experience}</h2>
+          <ul className="space-y-2 text-gray-300">
+            <li>• {t.experienceText}</li>
+
+            <div className="mt-6">
+              <h3 className="text-xl mb-4 text-gray-300">Skills</h3>
+              <div className="flex flex-wrap  gap-6 text-sm text-gray-400">
+                <span className="flex items-center gap-2">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg"
+                    className="w-10 h-10 object-contain"
+                  />
+                  PHP
+                </span>
+
+                <span className="flex items-center gap-2">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg"
+                    className="w-10 h-10 object-contain"
+                  />
+                  MySQL
+                </span>
+
+                <span className="flex items-center gap-2">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+                    className="w-10 h-10 object-contain"
+                  />
+                  React
+                </span>
+
+                <span className="flex items-center gap-2">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+                    className="w-10 h-10 object-contain"
+                  />
+                  Javascript
+                </span>
+
+                <span className="flex items-center gap-2">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jquery/jquery-original-wordmark.svg"
+                    className="w-10 h-10 object-contain"
+                  />
+                  JQuery
+                </span>
+
+                <span className="flex items-center gap-2">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg"
+                    className="w-10 h-10 object-contain"
+                  />
+                  Next.js
+                </span>
+              </div>
+            </div>
+            <div className="mt-6">
+              <h3 className="text-xl mb-4 text-gray-300">{t.study}</h3>
+              <div className="flex flex-wrap  gap-6 text-sm text-gray-400">
+                <span className="flex items-center gap-2">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"
+                    className="w-10 h-10 object-contain"
+                  />
+                  Java
+                </span>
+
+                <span className="flex items-center gap-2">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/reactnative/reactnative-original-wordmark.svg"
+                    className="w-10 h-10 object-contain"
+                  />
+                  React Native
+                </span>
+              </div>
+            </div>
+          </ul>
+        </div>
+
+        {/* OBJETIVOS */}
+        <div className="max-w-2xl w-full mb-10">
+          <h2 className="text-2xl mb-4 text-white-400">{t.goals}</h2>
+          <p className="text-gray-300">
+            {t.goalsText}
+          </p>
+        </div>
+
+        {/* HOBBIES */}
+        <div className="max-w-2xl w-full mb-10">
+          <h2 className="text-2xl mb-4 text-white-400">{t.beyondCode}</h2>
+          <p className="text-gray-300">
+            {t.beyondCodeText}
+          </p>
+        </div>
+
+        {/* PLAYLIST */}
+        <div className="max-w-2xl w-full">
+          <h2 className="text-2xl mb-4 text-yellow-400">{t.playlist}</h2>
+
+          <iframe
+            src="https://open.spotify.com/embed/playlist/11Dcs0DhQddZcPeSrd5QqD?si=9437cbb30eb945b7"
+            data-testid="embed-iframe"
+            width="100%"
+            height="352"
+            frameBorder="0"
+            allowFullScreen=""
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 }
--- src/lib/translations.ts
+++ src/lib/translations.ts
@@ -8,11 +8,16 @@ export const translations = {
     travel: "Travel",
     summon: "Summon",
     character: "Character",
-    // 
+    //
 
     //about
-    description:
-     "Passionate developer focused on creating interactive and immersive web experiences.",
+    aboutText: `I'm a Backend Developer with experience in PHP, MySQL, JavaScript and React, focused on building and maintaining modern, reliable web applications with a strong emphasis on user experience.
+
+      I have worked across both back-end and front-end development, implementing features, creating responsive interfaces, integrating APIs, and contributing to scalable solutions.
+
+      For four years at RadiusNet, I contributed to the maintenance and evolution of a management system for internet service providers, using PHP, Yii Framework, and MySQL. My work included bug fixing, feature improvements, and ongoing application support.
+
+      Prior to that, I worked at 2U, where I was responsible for maintaining and improving multiple web systems.`,
     stats: {
       UI: "UI",
       UX: "UX",
@@ -26,9 +31,19 @@ export const translations = {
     },
     level: "Nível",
     xp: "XP",
-    // 
+    experience: "Experience",
+    study: "Currently studyings",
+    playlist: "Favorite Playlist",
+    experienceText: "6 years studying and building web applications",
+    goals: "Goals",
+    goalsText: `Currently, I'm looking for opportunities where I can contribute to
+            impactful projects, collaborate with talented teams, and continue
+            growing as a developer.`,
+    beyondCode: "Beyond Code",
+    beyondCodeText: `Outside of coding, I enjoy games, mostly single-player (especially Souls-like), music, hiking and exploring creative ideas that can later become projects.`,
+    //
 
-    // projects 
+    // projects
     quests: "Quests",
     status: "Status",
     completed: "Completed",
@@ -50,12 +65,18 @@ export const translations = {
     levelUp: "Subir de Nivel",
     travel: "Viajar",
     summon: "Invocar",
-    // 
+    //
 
-    //  about 
+    //  about
     character: "Personagem",
-    description:
-      "Desenvolvedor apaixonado por criar experiências web interativas e imersivas.",
+    aboutText: `Sou um Desenvolvedor Backend com experiência em PHP, MySQL, JavaScript e React, focado em construir e manter aplicações web modernas e confiáveis, com forte atenção à experiência do usuário.
+
+      Tenho experiência atuando tanto no back-end quanto no front-end, implementando funcionalidades, criando interfaces responsivas, integrando APIs e contribuindo para soluções escaláveis.
+
+      Durante quatro anos na RadiusNet, trabalhei na manutenção e evolução de um sistema de gestão para provedores de internet, utilizando PHP, Yii Framework e MySQL. Minhas atividades incluíam correção de bugs, melhorias e suporte contínuo da aplicação.
+
+      Antes disso, trabalhei na 2U, onde fui responsável pela manutenção e evolução de diversos sistemas web.`,
+
     stats: {
       UI: "Interface",
       UX: "Experiência",
@@ -69,6 +90,15 @@ export const translations = {
     },
     level: "Level",
     xp: "XP",
+    experience: "Experiência",
+    study: "Estudando",
+    playlist: "Playlist Favorita",
+    experienceText: "6 anos estudando e construindo aplicações web",
+    goals: "Objetivo",
+    goalsText: `Atualmente estou em busca de oportunidades onde eu possa contribuir para projetos relevantes, colaborar com equipes talentosas e continuar evoluindo como desenvolvedor.`,
+    beyondCode: "Alem do Código",
+    beyondCodeText: `Outside of coding, I enjoy games, mostly single-player (especially Souls-like), music, hiking and exploring creative ideas that can later become projects.`,
+    //
     //
 
     // projects
